8d45591f4218ef2386cfb29d5afc8b690f4a162d
Ashish Mishra <ashish.mishra.sse@gmail.com>
2024-12-02 16:17:49 +0530

    Variables and Loops

---
+++ src/Day4/Statements.java
@@ -0,0 +1,17 @@
+package Day4;
+
+public class Statements
+{
+    public static void main(String[] args)
+    {
+        int age= 12;
+        if(age>=18)
+        {
+            System.out.println("He can vote");
+        }
+        else
+        {
+            System.out.println("Not eligible to Vote");
+        }
+    }
+}
---
+++ src/Day4/Variables.java
@@ -0,0 +1,27 @@
+package Day4;
+
+public class Variables {
+    public static void main(String[] args) {
+        byte x = 1;
+        //Primitive Data Type
+        //[byte(1 Byte=8 bits(-128 to +127)), short(2 Byte(-32768 to 32767)),int(4 Byte) ,long(8 Byte)], [float(4 Byte), double(8 Byte)], char(2 Byte), boolean(~1 bit)
+
+        short s = 32767;
+        int s1 = 32768;
+
+        long i1 = 2147483648l;
+
+        short sh = 126;
+        byte b2 = 8;
+        int sum = sh + b2;
+
+        double f1 = 12.3564564554564653;
+        System.out.println(f1);
+
+        char c1 ='y';
+        boolean b1 =false;
+
+       String str= "Shubham";
+
+    }
+}
